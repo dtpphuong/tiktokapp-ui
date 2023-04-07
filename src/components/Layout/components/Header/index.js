7428193576263800b8dbb260a22e2713b256a667
Phuong <duongthanhphuong7413@gmail.com>
2023-04-07 21:47:36 +0700

    [header UI]

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -2,28 +2,43 @@
 import { useEffect, useState } from 'react'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCircleXmark, faSpinner, faMagnifyingGlass, faEllipsisVertical, faEarthAsia, faCircleQuestion, faKeyboard } from '@fortawesome/free-solid-svg-icons'
+import { faCircleXmark, faSpinner, faMagnifyingGlass, faEllipsisVertical, faEarthAsia, faCircleQuestion, faKeyboard, faCloudArrowUp, faMessage, faUser, faCoins, faGear, faSignOut } from '@fortawesome/free-solid-svg-icons'
 import { Wrapper as PopperWrapper } from '~/components/Popper'
 import classNames from 'classnames/bind'
 
 import styles from './Header.module.scss'
 import images from '~/assets/images'
-import Tippy from '@tippyjs/react/headless'
+import Tippy from '@tippyjs/react'
+import 'tippy.js/dist/tippy.css';
+import HeadlessTippy from '@tippyjs/react/headless'
 import AccountItem from '~/components/AccountItem'
 import Button from '~/components/Button'
 import Menu from '~/components/Popper/Menu'
-
+import Image from '~/components/Images'
 
 const cx = classNames.bind(styles)
 const MENU_ITEMS = [
     {
         icon: <FontAwesomeIcon icon={faEarthAsia} />,
         title: 'English',
+        children: {
+            title: 'Language',
+            data: [
+                {
+                    code: 'en',
+                    title: 'English'
+                },
+                {
+                    code: 'vi',
+                    title: 'Tiếng Việt'
+                },
+            ]
+        }
     },
     {
         icon: <FontAwesomeIcon icon={faCircleQuestion} />,
         title: 'Feelback and Help',
-        to:'/feelback'
+        to: '/feelback'
     },
     {
         icon: <FontAwesomeIcon icon={faKeyboard} />,
@@ -32,19 +47,46 @@ const MENU_ITEMS = [
 ];
 
 function Header() {
-    const [searchResult, setSearchResult] = useState([])
-
+    const [searchResult, setSearchResult] = useState([]);
+    const currentUser = true;
     useEffect(() => {
         setTimeout(() => {
             setSearchResult([1, 2, 3])
         }, 0)
     }, [])
+    //HanleLogic
+    const handleMenuChange = (menuitem) => {};
+    
+    const userMenu = [
+        {
+            icon: <FontAwesomeIcon icon={faUser} />,
+            title: 'View profile',
+            to: '/@hoaa'
+        },
+        {
+            icon: <FontAwesomeIcon icon={faCoins} />,
+            title: 'Get coins',
+            to: '/coins'
+        },
+        {
+            icon: <FontAwesomeIcon icon={faGear} />,
+            title: 'Settings',
+            to: '/setting'
+        },
+        ...MENU_ITEMS,
+        {
+            icon: <FontAwesomeIcon icon={faSignOut} />,
+            title: 'logout',
+            to: '/logout',
+            separate: true,
+        },
+    ]
 
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
                 <img src={images.logo} alt="Tiktok" />
-                <Tippy
+                <HeadlessTippy
                     interactive
                     // 
                     disabled
@@ -71,14 +113,30 @@ function Header() {
                             <FontAwesomeIcon icon={faMagnifyingGlass} />
                         </button>
                     </div>
-                </Tippy>
+                </HeadlessTippy>
                 <div className={cx('actions')}>
-                    <Button text >Upload</Button>
-                    <Button primary>Log in</Button>
-                    <Menu items={MENU_ITEMS} >
-                        <button className={cx('more-btn')}>
-                            <FontAwesomeIcon icon={faEllipsisVertical} />
-                        </button>
+                    {currentUser ? (
+                        <Tippy delay={[0, 200]} content="Upload video">
+                            <button className={cx('action-btn')}>
+                            <FontAwesomeIcon icon={faCloudArrowUp} />
+                            </button>
+                        </Tippy>
+                    ) : (
+                        <>
+                            <Button text >Upload</Button>
+                            <Button primary>Log in</Button>
+                        </>
+                    )}
+                    <Menu items={currentUser ? userMenu : MENU_ITEMS} onChange={handleMenuChange} >
+                        {currentUser ? (
+                            <Image className={cx('user-avatar')} 
+                                src='https://haycafe.vn/wp-content/uploads/2021/12/hinh-anh-avatar-dep-cho-con-gai-dai-dien-Facebook-Zalo-Tiktok.jpg' 
+                                alt="Nguyen Van A" />
+                        ) : (
+                            <button className={cx('more-btn')}>
+                                <FontAwesomeIcon icon={faEllipsisVertical} />
+                            </button>
+                        )}
                     </Menu>
                 </div>
             </div>
--- src/components/Popper/Menu/index.js
+++ src/components/Popper/Menu/index.js
@@ -3,27 +3,51 @@ import Tippy from '@tippyjs/react/headless';
 import classNames from 'classnames/bind'
 import styles from './Menu.module.scss'
 import MenuItem from './MenuItem';
+import Header from './Header'
+import { useState } from 'react';
 
 const cx = classNames.bind(styles);
 
-function Menu({ children, items = [] }) {
+const defaultFn = () =>{
+
+}
+
+function Menu({ children, items = [], onChange = defaultFn }) {
+
+    const [history, setHistory] = useState([{data: items}]);
+    const current = history[history.length - 1];
 
     const renderItem = () => {
-        return items.map((i, index) => <MenuItem key={index} data={i} />)
-    }
+        return current.data.map((i, index) => {
+            const isParent = !!i.children
+            return <MenuItem key={index} data={i} onClick={() =>{
+                if(isParent) {
+                    setHistory((prev) => [...prev, i.children])
+                } else {
+                    onChange(i)
+                }
+            }} />
+        });
+    };
 
     return (
         <Tippy
-        visible
             interactive
+            delay={[0, 700]}
+            offset={[12,8]}
             placement='bottom-end'
             render={(attrs) => (
                 <div className={cx('menu-list')} tabIndex="-1" {...attrs}>
                     <PopperWrapper className={cx('menu-popper')}>
+                    {history.length > 1 && <Header title="Language" onBack={() => {
+                        setHistory(prev => prev.slice(0, prev.length - 1))
+                    }} />}
                         {renderItem()}
                     </PopperWrapper>
                 </div>
-            )}>
+            )}
+            onHide={() => setHistory(prev => prev.slice(0,1))}
+            >
             {children}
         </Tippy>
     );
